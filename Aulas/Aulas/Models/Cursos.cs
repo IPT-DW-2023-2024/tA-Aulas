773c7ed0eebd345203ee002c68e8fec98972e078
Jose Casimiro Pereira <casimiro@ipt.pt>
2024-04-10 10:43:33 +0100

    alterações na interface da view Create Curso + modelo Cursos

--- Aulas/Aulas/Models/Cursos.cs
+++ Aulas/Aulas/Models/Cursos.cs
@@ -11,9 +11,14 @@ namespace Aulas.Models {
       [Key] // PK
       public int Id { get; set; }
 
+      [Required(ErrorMessage ="O {0} é de preenchimento obrigatório!")]
+      [StringLength(100)]
       public string Nome { get; set; }
 
-      public string Logotipo { get; set; }
+
+      [Display(Name ="Logótipo")] // define o nome a aparecer no ecrã
+      [StringLength(50)]
+      public string? Logotipo { get; set; } // o '?' vai tornar o atributo em preenchimento facultativo
 
       /* ****************************************
        * Construção dos Relacionamentos
