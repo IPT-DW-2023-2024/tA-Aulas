b38a53279e0d8ece0678e0db4875d9a2ecd398a4
Jose Casimiro Pereira <casimiro@ipt.pt>
2024-05-08 09:33:26 +0100

    migração para a criação do atributo Telemovel + alteração da obrigatoriedade de preenchimento de strings

---
+++ Aulas/Aulas/Data/Migrations/20240508083054_AdicaoTelemovelUtilizador.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aulas.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicaoTelemovelUtilizador : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Utilizadores",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Telemovel",
+                table: "Utilizadores",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "UCs",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Telemovel",
+                table: "Utilizadores");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Utilizadores",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "UCs",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
