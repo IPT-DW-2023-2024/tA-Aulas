cad8c73fd216b8eedab5c47da95979e8f89e68da
Jose Casimiro Pereira <casimiro@ipt.pt>
2024-05-08 10:32:37 +0100

    uso de Regular Expressions no Telemóvel

--- Aulas/Aulas/Models/Utilizadores.cs
+++ Aulas/Aulas/Models/Utilizadores.cs
@@ -6,12 +6,16 @@ namespace Aulas.Models {
       [Key] // PK
       public int Id { get; set; }
 
+      [StringLength(50)]
+      [Required(ErrorMessage ="O {0} é de preenchimento obrigatório.")]
       public string Nome { get; set; }
 
       [Display(Name ="Data Nascimento")]
       public DateOnly DataNascimento { get; set; }
 
       [Display(Name ="Telemóvel")]
+      [StringLength(19)]
+      [RegularExpression("([+]|00)?[0-9]{6,17}", ErrorMessage ="o {0} só pode conter digitos. No mínimo 6.")]
       public string Telemovel { get; set; }
 
    }
